b6ffc4f4df842db7949041c24b296bd7713370ce
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-11-21 15:58:45 +0000

    formatting

--- consultations/apps.py
+++ consultations/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class ConsultationsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'consultations'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "consultations"
