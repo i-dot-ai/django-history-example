99af71752a6928e9caebb1f0801d75e195ebd4a7
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-11-21 20:26:02 +0000

    Add simple history to settings. Middleware trackes users automatically.

--- consult/settings.py
+++ consult/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "simple_history",
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "simple_history.middleware.HistoryRequestMiddleware",
 ]
 
 ROOT_URLCONF = "consult.urls"
