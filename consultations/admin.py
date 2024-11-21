44225e2e89090ba09f067ae29b74d5dc19662c96
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-11-21 21:08:06 +0000

    Add some initial models, some with history.

--- consultations/admin.py
+++ consultations/admin.py
@@ -1,1 +1,6 @@
-# Register your models here.
+from django.contrib import admin
+
+from consultations.models import Execution, Theme
+
+admin.site.register(Execution)
+admin.site.register(Theme)
--- consultations/models.py
+++ consultations/models.py
@@ -1,1 +1,37 @@
-# Create your models here.
+import uuid
+
+from django.db import models
+from simple_history.models import HistoricalRecords
+
+
+class UUIDPrimaryKeyModel(models.Model):
+    class Meta:
+        abstract = True
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(editable=False, auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class Execution(TimeStampedModel, UUIDPrimaryKeyModel):
+    TYPE_CHOICES = [
+        ("sentiment", "Sentiment"),
+        ("theme_generation", "Theme Generation"),
+        ("theme_mapping", "Theme Mapping"),
+        ("other", "Other"),
+    ]
+    type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+    description = models.TextField()
+
+
+class Theme(TimeStampedModel, UUIDPrimaryKeyModel):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    execution = models.ForeignKey(Execution, on_delete=models.CASCADE)
+
+    history = HistoricalRecords()
