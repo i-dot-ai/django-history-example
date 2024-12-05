3cac5c6613da51a1077a5d2f55e35108c9f835ba
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-12-05 17:29:26 +0000

    add response mapping form

--- consult/consultations/forms.py
+++ consult/consultations/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import FrameworkTheme, Theme
+from .models import FrameworkTheme, Theme, ResponseMapping, Response
 
 
 class ThemeForm(forms.ModelForm):
@@ -15,4 +15,18 @@ class FrameworkForm(forms.ModelForm):
         fields = ["name", "description"]
 
 
+class ResponseMappingForm(forms.ModelForm):
+    response = forms.CharField(label="Response", required=False, widget=forms.Textarea(attrs={'readonly': 'readonly'}))
+
+    class Meta:
+        model = ResponseMapping
+        fields = ["framework_theme", "response"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance and self.instance.pk:
+            self.fields['response'].initial = self.instance.response.response
+
+
 FrameworkFormSet = forms.modelformset_factory(FrameworkTheme, form=FrameworkForm, extra=3)
+
